5d1667a5ed8157568311f418abc0e972c966e22e
Jan Bednařík <jan.bednarik@gmail.com>
2020-01-21 21:01:00 +0100

    Working members count

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 import falcon
+import requests
+import re
+
+
+MEMBERS_LIST_URL = "https://forum.pirati.cz/memberlist.php"
+
+
+def get_members_list_page():
+    response = requests.get(MEMBERS_LIST_URL)
+    return response.text
+
+
+def parse_members_count(page):
+    m = re.search(r"[\d]+(?= uživatelů)", page)
+    return int(m.group(0))
 
 
 class MembersResource:
     def on_get(self, req, resp):
         """Handles GET requests"""
-        results = {"members": 1234}
+        page = get_members_list_page()
+        count = parse_members_count(page)
+        results = {"members": count}
         resp.media = results
 
 
--- tests.py
+++ tests.py
@@ -9,6 +9,12 @@ def client():
     return testing.TestClient(app.create_api())
 
 
-def test_get_members(client):
+def test_parse_members_count():
+    text = """foo bar 123456 uživatelů asd fgh"""
+    assert app.parse_members_count(text) == 123456
+
+
+def test_get_members(client, mocker):
+    mocker.patch("app.get_members_list_page", return_value="1234 uživatelů")
     result = client.simulate_get("/members")
     assert result.json == {"members": 1234}
